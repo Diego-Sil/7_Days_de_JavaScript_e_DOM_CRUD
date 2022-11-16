a554282fdcee1de9ad7c395268996956a2796e13
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-11-16 16:13:00 -0300

    Inserido: função criatr e função criatd e estão inserindo novos registros na tabela

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 let form = document.querySelector(".js-form");
+let tabela = document.querySelector('.js-table')
+
 
 form.addEventListener("submit",(e)=>{
   e.preventDefault()
-  exibeInformacao()
-});
-
-function exibeInformacao(){
   let nome = form.nome.value;
   let dataNasc = form.dataNascimento.value;
-  // console.log(nome, dataNasc);
   const novaPessoa = new pessoa (nome, dataNasc)
   
   console.log(novaPessoa);
-}
+  tabela.appendChild(criaTr(novaPessoa))
+});
+
+
 class pessoa  {
   constructor(nome, dataNasc){
   this.nome = nome;
   this.dataNascimento = dataNasc;
   }
+}
+
+function criaTd(dado){
+  const td = document.createElement('td')
+  td.textContent = dado
+  return td
+}
+
+function criaTr(pessoa){
+
+  let tr = document.createElement('tr')
+  tr.appendChild(criaTd(pessoa.nome))
+  tr.appendChild(criaTd(pessoa.dataNascimento))
+  return tr
 }
