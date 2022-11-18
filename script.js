170dfbcff0b00ef9e681b6b4196ba2465a5be79d
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-11-17 22:56:58 -0300

    Inserido: botão Altualiza em cada linha da tabela

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let form = document.querySelector('.js-form');
 let tabela = document.querySelector('.js-table');
-let arrPessoa = [];
+let arrPessoa = JSON.parse(localStorage.getItem('pessoa')) || [];
+
+arrPessoa.forEach((element) =>{
+  criaElemento(element)
+})
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault()
   let nome = form.nome.value;
   let dataNasc = form.dataNascimento.value;
-
+  
   const pessoaAtual = {
     'nome': nome,
     'dataNascimento': dataNasc    
   }
+ 
   salvaLocalmente(pessoaAtual)
   criaElemento(pessoaAtual)
   limpaCampos()
@@ -29,10 +34,13 @@ function limpaCampos(){
 function criaElemento(pessoa){
   const tdNome = document.createElement('td')
   const tdDateNasc = document.createElement('td')
-  tdNome.textContent = pessoa.nome
-  tdDateNasc.textContent = pessoa.dataNascimento
+  tdNome.innerHTML = pessoa.nome
+  tdDateNasc.innerHTML = pessoa.dataNascimento
+  const btnAtualiza = document.createElement('button')
+  btnAtualiza.innerHTML = 'Atualiza'
   const trElemento = document.createElement('tr')
   trElemento.appendChild(tdNome)
   trElemento.appendChild(tdDateNasc)
+  trElemento.appendChild(btnAtualiza)
   tabela.appendChild(trElemento)
 }
