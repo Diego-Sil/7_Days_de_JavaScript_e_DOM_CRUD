d7bed194d41550e645df5aa24327cc332fdcc826
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-11-22 12:40:44 -0300

    Os botões de delete e salva estão funcionando e o atualizar mostra o elemento nos campos do form, o id esta iterando muito bem

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ arrPessoa.forEach((element) =>{
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault()
+ 
   let nome = form.nome.value;
   let dataNasc = form.dataNascimento.value;
-  let id = arrPessoa.length
-  
+  const existe = arrPessoa.find( elemento => elemento.nome === nome.value )
+
   const pessoaAtual = {
     'nome': nome,
     'dataNascimento': dataNasc,
-    'id': id   
+    'id': '',
   }
- 
+
+  if (existe) {
+    pessoaAtual.id = existe.id
+    
+    atualizaElemento(pessoaAtual,existe.nome,existe.dataNasc)
+
+    arrPessoa[arrPessoa.findIndex(elemento => elemento.id === existe.id)] = pessoaAtual
+} else {
+    pessoaAtual.id = arrPessoa[arrPessoa.length -1] ? (arrPessoa[arrPessoa.length-1]).id + 1 : 0;
+  }
+  
   salvaLocalmente(pessoaAtual)
   criaElemento(pessoaAtual)
   limpaCampos()
@@ -26,6 +37,11 @@ form.addEventListener('submit',(e)=>{
 function salvaLocalmente(pessoa){
   arrPessoa.push(pessoa)
   localStorage.setItem('pessoa',JSON.stringify(arrPessoa))  
+}
+
+function atualizaElemento(pessoa) {
+    arrPessoa[pessoa.id].nome = pessoa.nome
+    arrPessoa[pessoa.id].dataNasc = pessoa.dataNasc
 }
 
 function limpaCampos(){
@@ -38,11 +54,42 @@ function criaElemento(pessoa){
   const tdDateNasc = document.createElement('td')
   tdNome.innerHTML = pessoa.nome
   tdDateNasc.innerHTML = pessoa.dataNascimento
-  const btnAtualiza = document.createElement('button')
-  btnAtualiza.innerHTML = 'Atualiza'
   const trElemento = document.createElement('tr')
   trElemento.appendChild(tdNome)
   trElemento.appendChild(tdDateNasc)
-  trElemento.appendChild(btnAtualiza)
+  trElemento.appendChild(criaBotaoAtualiza(pessoa))
+  trElemento.appendChild(criaBotaoDeleta(pessoa.id))
+
   tabela.appendChild(trElemento)
+}
+
+function criaBotaoAtualiza(pessoa){
+  const btnAtualiza = document.createElement('button')
+  btnAtualiza.innerHTML = 'Atualiza'
+  btnAtualiza.addEventListener('click', function(){
+    exibePessoa(pessoa)
+  })
+  return btnAtualiza
+}
+
+function exibePessoa(pessoa){
+  form.nome.value = pessoa.nome;
+  form.dataNascimento.value = pessoa.dataNascimento;
+  const existe = arrPessoa.find(element => element.id === pessoa.id)
+}
+
+function criaBotaoDeleta(id){
+  const btnDeleta = document.createElement('button')
+  btnDeleta.innerHTML = 'Deletar'
+  btnDeleta.addEventListener('click', function(){
+    deletaElemento(this.parentNode,id)
+  })
+  return btnDeleta
+}
+
+function deletaElemento(tag, id){
+  tag.remove()
+  arrPessoa.splice(arrPessoa.findIndex(elemento => elemento.id === id), 1)
+
+  localStorage.setItem('pessoa',JSON.stringify(arrPessoa))  
 }
